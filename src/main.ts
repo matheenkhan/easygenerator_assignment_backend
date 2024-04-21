9c0471ac8b53c1e974eee23f5d9c3528c4d5aa48
Matheen Khan <matheen.khan12@gmail.com>
2024-04-22 03:33:23 +0530

    v1

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
   app.use(cors());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
